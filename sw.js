708bc30c021f6cdf367d93b33da77ba691563699
Sean <alphapoptart@users.noreply.github.com>
2026-08-12 22:58:54 -0400

    Bump v2 offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='glasshouse-letters-full-v2-20260813';
+const CACHE='glasshouse-letters-full-v2-20260813b';
 const ASSETS=['./','index.html','core.css','v2.css','manifest.json','icon.svg','icon-192.png','js/storage.js','js/data.js','js/audio.js','js/plantart.js','js/game.js','js/minigames.js','js/visit.js','js/ui.js','js/sequel-features.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
